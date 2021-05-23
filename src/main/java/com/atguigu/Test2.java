dc1a984cf7e60155cf456612d7fb232f54290518
q2452006628 <54787685+q2452006628@users.noreply.github.com>
2021-05-23 09:04:51 +0800

    Update Test2.java

--- src/main/java/com/atguigu/Test2.java
+++ src/main/java/com/atguigu/Test2.java
@@ -5,5 +5,6 @@ public class Test2 {
         System.out.println("袁老一路走好！");
         System.out.println("五月二十二值得纪念的日子");
         System.out.println("潘嘎之交");
+        System.out.println("嘎子，你把持不住");
     }
 }
